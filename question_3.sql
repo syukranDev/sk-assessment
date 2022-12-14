ce71db8eb5d0fc6c9b96e50a935870d3b0f13a75
syukranDev <syukran.dev@gmail.com>
2022-12-14 21:03:13 +0800

    q3a q3b fixes

--- question_3.sql
+++ question_3.sql
@@ -2,15 +2,18 @@
 -- Question 3-1 : Database Query
 -- Write an SQL statement that returns the users’ id, name and total sale amount for users who have total sales above 1000.
 
-SELECT S.user_id, U.name, 
+SELECT S.user_id, U.name, S.sale_amount
 FROM users U
 INNER JOIN sales S ON U.id = S.user_id
 WHERE sale_amount >= 1000
 
 
+
 -- Question 3-2 : Database Query
 --Write an SQL statement that returns the average sales for users born in 80s, 90s and 2000s
 
-SELECT  U.name, AVG(birth_year)
+SELECT U.name, AVG(sale_amount) AS average_total_sales
 FROM users U
 INNER JOIN sales S ON U.id = S.user_id
+WHERE U.birth_year >= 1980
+GROUP BY U.name
