3762cee0289112b96ca1bbd462cef1a9aac8a51a
syukranDev <syukran.dev@gmail.com>
2022-12-14 21:43:14 +0800

    fix q3a & q3b solution and provide result in commented table form

--- question_3.sql
+++ question_3.sql
@@ -2,11 +2,21 @@
 -- Question 3-1 : Database Query
 -- Write an SQL statement that returns the users’ id, name and total sale amount for users who have total sales above 1000.
 
-SELECT  U.name, SUM(S.sale_amount) AS total_sales_amount
+SELECT  S.user_id, U.name, SUM(S.sale_amount) AS total_sales_amount
 FROM users U
 INNER JOIN sales S ON U.id = S.user_id
-GROUP BY U.name
+GROUP BY U.name, S.user_id
 HAVING SUM(S.sale_amount) >= 1000
+
+
+--  Expected result :
+-- ======================================
+-- | user_id | name | total_sales_amount
+-- ======================================
+-- |     1   | Adam |       1200        |
+-- |     2   | Bob  |       1700        |
+-- |     5   | Eddy |       2100        |
+-- ======================================
 
 
 
@@ -18,3 +28,15 @@ FROM users U
 INNER JOIN sales S ON U.id = S.user_id
 WHERE U.birth_year >= 1980
 GROUP BY U.name
+
+
+--  Expected result :
+-- ==================================
+-- |   name    | average_total_sales
+-- ==================================
+-- |    Bob    |       850          |
+-- |    Eddy   |        1050        |
+-- |    Adam   |        400         |
+-- |    Salman |        450         |
+-- =================================
+
